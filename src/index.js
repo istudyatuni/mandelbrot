d662e3709be65f767b2be5b29793da3e356a21bf
Ilia <istudyatuni@gmail.com>
2022-02-06 10:23:05 +0300

    fix: HMR snippet not work

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ const app = new App({
 
 export default app
 
+// hmm, removing this not removed HMR
+
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
-if (import.meta.hot) {
-	import.meta.hot.accept()
-	import.meta.hot.dispose(() => {
-		app.$destroy()
-	})
-}
+// if (import.meta.hot) {
+// 	import.meta.hot.accept()
+// 	import.meta.hot.dispose(() => {
+// 		app.$destroy()
+// 	})
+// }
