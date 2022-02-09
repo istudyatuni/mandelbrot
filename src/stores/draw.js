328a2e3eea7ccd1a784114950d7d1b846dc08f4e
Ilia <istudyatuni@gmail.com>
2022-02-09 12:30:31 +0300

    style: update default scale

--- src/stores/draw.js
+++ src/stores/draw.js
@@ -1,3 +1,3 @@
 import { sessionStore } from 'svelte-storages'
 
-export const settings = sessionStore('draw-settings', { lx: -5, rx: 3 })
+export const settings = sessionStore('draw-settings', { lx: -3, rx: 1 })
