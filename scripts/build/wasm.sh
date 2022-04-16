7f9cc4da0204e4e1a93fd7bf585834769ccad685
Ilia <istudyatuni@gmail.com>
2022-04-16 20:25:29 +0300

    build: add build system and try to fix path if command not found

--- scripts/build/wasm.sh
+++ scripts/build/wasm.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 cd wasm
-wasm-pack build --release --out-dir=../src/wasm
+
+build () {
+	wasm-pack build --release --out-dir=../src/wasm
+}
+
+build || export PATH=$PATH:$HOME/.cargo/bin && build
