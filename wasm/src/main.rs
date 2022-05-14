ae36b2ae4268b33a87fb125cda08ed3bb6f962d1
Ilia <istudyatuni@gmail.com>
2022-05-14 13:48:23 +0300

    test: add simple benchmark

---
+++ wasm/src/main.rs
@@ -0,0 +1,28 @@
+mod lib;
+
+fn main() {}
+
+#[test]
+fn benchmark() {
+    use lib::Mandelbrot;
+    use std::time::Instant;
+
+    const W: usize = 1600;
+    const H: usize = 752;
+
+    let mut m = Mandelbrot::new(W * H);
+
+    let now = Instant::now();
+
+    // fully black 'lx=-0.13852233886718768&rx=-0.13754577636718768&yc=0.8637609863281248' ~ 4.5s
+    m.calc(
+        -0.13852233886718768,
+        -0.13754577636718768,
+        0.8637609863281248,
+        W as u16,
+        H as u16,
+    );
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+}
